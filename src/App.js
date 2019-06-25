354a1883d213b0c74006425859d3f71dd0382393
delapria <delapria@gmail.com>
2019-06-24 21:08:39 -0300

    instalation an configuration reactotron

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import './config/reactotron';
 import GlobalStyle from './styles/global';
+
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import Header from './components/Header';
